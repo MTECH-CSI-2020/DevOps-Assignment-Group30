407b32289405779af8ccfc7ffa1421682c0ca898
administrator@SPARTANS.LOCAL <administrator@SPARTANS.LOCAL>
2021-10-10 10:09:35 -0700

    New deck From File Function Added

--- decks.go
+++ decks.go
@@ -29,3 +29,14 @@ func newDeck() deck {
 	return cards
 
 }
+
+func newDeckFromFile(filename string) deck {
+	bs, err := ioutil.ReadFile(filename)
+	if err != nil {
+		fmt.Println("Error: ", err)
+		os.Exit(1)
+	}
+	s := strings.Split(string(bs), ",")
+	return deck(s)
+}
+
